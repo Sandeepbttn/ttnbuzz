5e184eda61c76b50e366dc7e6e22015b0be422ef
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-10 15:49:23 +0530

    first commit , setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <p>hello world</p>
+
+      <form action="/home/sandeep/VSCode/TTN-Buzz/server/index.js/post" method="post" 
+              className="form">
+          <button type="submit">Connected?</button>
+        </form>
+    </div>
+  );
+}
+
+export default App;
