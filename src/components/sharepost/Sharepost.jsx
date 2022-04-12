913585863518e42c402b14041930d2a5906ce3e3
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-12 13:52:53 +0530

    adding icon and option in sharepost

--- src/components/sharepost/Sharepost.jsx
+++ src/components/sharepost/Sharepost.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './sharepost.css'
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+
 export default function Sharepost() {
   return (
 
@@ -9,7 +11,10 @@ export default function Sharepost() {
                     <div className="sharepostTop">
                         <img className="shareProfileImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSD8r54g86MEGRzywLkkTTu8CVFuhz3XdIGGA&usqp=CAU" alt="profile image" />
                         <input placeholder="Start a post" className="sharepostInput"/>
+                        <AddPhotoAlternateIcon className="sharepostIcon"/>
+                        <span className="sharepostOptionText">Photo/Video</span>
                     </div>   
+                    
             </div>
         </div>
   )
