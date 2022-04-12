c575fd5167870b09fa8c27beec4afad2292de714
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-12 11:34:59 +0530

    import suggestion and contacts in rightbar section

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './rightbar.css'
+import Suggestion from '../suggestion/Suggestion'
+import Contacts from '../contacts/Contacts'
 
 export default function Rightbar() {
   return (
-    <div className='rightbar'>Rightbar</div>
+    <div className='rightbar'>Rightbar
+       <div className="contactsbox">
+         <Contacts/>
+       </div>
+       <div className="suggestionbox">
+         <Suggestion/>
+       </div>
+    </div>
   )
 }
