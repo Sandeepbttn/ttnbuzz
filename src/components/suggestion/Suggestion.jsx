de915404556453ddf257963740bc6e32ef81421c
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-12 12:30:46 +0530

    seacrh icon adjustment

--- src/components/suggestion/Suggestion.jsx
+++ src/components/suggestion/Suggestion.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import './suggestion.css'
 
 export default function suggestion() {
   return (
-    <div>suggestion</div>
+    <div className="suggestion">
+      <div>
+      <h3>Suggestions</h3>
+      </div>
+      
+
+      <div className="suggestionDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className="suggestionImg"/>
+            <span className="suggestionName">Sandeep Bhatt</span>
+            <button>Add Friend +</button>
+        </div>
+        
+        <div className="suggestionDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className="suggestionImg"/>
+            <span className="suggestionName">Sandeep Bhatt</span>
+            <button>Add Friend +</button>
+        </div>
+        <div className="suggestionDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className="suggestionImg"/>
+            <span className="suggestionName">Sandeep Bhatt</span>
+            <button>Add Friend +</button>
+        </div>
+        <div className="suggestionDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className="suggestionImg"/>
+            <span className="suggestionName">Sandeep Bhatt</span>
+            <button>Add Friend +</button>
+        </div>
+        <div className="suggestionDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className="suggestionImg"/>
+            <span className="suggestionName">Sandeep Bhatt</span>
+            <button>Add Friend +</button>
+        </div>
+        <div className="suggestionDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className="suggestionImg"/>
+            <span className="suggestionName">Sandeep Bhatt</span>
+            <button>Add Friend +</button>
+        </div>
+        
+    </div>
   )
 }
