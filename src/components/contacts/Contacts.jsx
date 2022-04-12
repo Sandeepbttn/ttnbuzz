43d8a46b617fe4d4807bf06bb45eec2805afd8af
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-12 12:11:31 +0530

    adding searchbar in contacts

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './contacts.css'
+import { PersonSearch } from '@mui/icons-material'
+import { display } from '@mui/system'
 
 export default function Contacts() {
   return (
     <div className="contacts">
+        <h2>Contacts</h2>
+        <div className="searchbar">
+             <PersonSearch className='searchIcon'/>
+             <input placeholder='find friends' className='searchInput' />
+         </div>
         <div className="contactDetails">
             <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className='contactsImg'/>
             <span className="contactsName">Sandeep Bhatt</span>
