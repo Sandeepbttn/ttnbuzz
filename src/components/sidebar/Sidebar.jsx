9561013ae70df61e02c371727c794794cac28784
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-12 18:13:38 +0530

    adding sections in sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,22 @@ import './sidebar.css'
 
 export default function Sidebar() {
   return (
-    <div className="sidebar">Sidebar</div>
+    <div className="sidebar">
+      <div className="userMiniProfile">
+        <h1>user mini profile</h1>
+      </div>
+
+      <div className="sidebarBox">
+        <div className="sidebarBoxRecent">
+          <h3>Recent</h3>
+        </div>
+        <div className="sidebarBoxGroups">
+            <h3>Groups</h3>
+        </div>
+        <div className="sidebarBoxSub">
+            <h3>Subscriptions</h3>
+        </div>
+      </div> 
+    </div>
   )
 }
