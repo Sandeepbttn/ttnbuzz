07c014c07ec7d54293cbfdf5b3432bebff4e935f
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-12 11:58:13 +0530

    adding static user

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import './contacts.css'
 
 export default function Contacts() {
   return (
-    <div>Contacts</div>
+    <div className="contacts">
+        <div className="contactDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className='contactsImg'/>
+            <span className="contactsName">Name : Sandeep Bhatt</span>
+        </div>
+        <div className="contactDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className='contactsImg'/>
+            <span className="contactsName">Name : Sandeep Bhatt</span>
+        </div>
+        <div className="contactDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className='contactsImg'/>
+            <span className="contactsName">Name : Sandeep Bhatt</span>
+        </div>
+        <div className="contactDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className='contactsImg'/>
+            <span className="contactsName">Name : Sandeep Bhatt</span>
+        </div>
+        <div className="contactDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className='contactsImg'/>
+            <span className="contactsName">Name : Sandeep Bhatt</span>
+        </div>
+        <div className="contactDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className='contactsImg'/>
+            <span className="contactsName">Name : Sandeep Bhatt</span>
+        </div>
+        <div className="contactDetails">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="profile image"  className='contactsImg'/>
+            <span className="contactsName">Name : Sandeep Bhatt</span>
+        </div>
+        
+    </div>
   )
 }
