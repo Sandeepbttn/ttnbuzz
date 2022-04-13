6a4780c00dc5a0dfdf403716726410d954b87144
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-13 15:08:29 +0530

    adding axios

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Register from "./pages/register/Register";
 function App() {
   return (
     <>
-      <Home/>
-      {/* <Login/> */}
+      {/* <Home/> */}
+      <Login/>
       {/* <Register/> */}
     </>
   )
