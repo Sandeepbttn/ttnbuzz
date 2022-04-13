47bfb289d7fc435a9797fe4d9f98b188fdcc84bf
salonibhatia-ttn <saloni.bhatia@tothenew.com>
2022-04-13 20:48:09 +0530

    implemented react routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import { BrowserRouter as Router, Route, Routes} 
+        from "react-router-dom";
+
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-
-
+import Profile from "./pages/profile/Profle";
 
 function App() {
   return (
-    <>
-      <Home/>
-      {/* <Login/> */}
-      {/* <Register/> */}
-    </>
+    <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </Routes>
+    </Router>
   )
 }
 
