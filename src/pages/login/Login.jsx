25fe1b6f4e6007543d916fba2f885682875368bb
sandeepbttn <sandeep.bhatt1@tothenew.com>
2022-04-18 21:28:31 +0530

    updating login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import './login.css'
 
 export default function Login() {
+
+  const handleClick=(e)=>{
+    e.preventDefault();
+    console.log("click")
+
+  }
     return (
       <div className="login">
         <div className="loginWrapper">
@@ -13,15 +19,15 @@ export default function Login() {
           <button className="loginButton">Sign in with Google</button>
         </div>
           <div className="loginRight">
-            <div className="loginBox">
-              <input placeholder="Email" className="loginInput" />
-              <input placeholder="Password" className="loginInput" />
+            <form className="loginBox"  onSubmit={handleClick}>
+              <input placeholder="Email" type="email " className="loginInput" required/>
+              <input placeholder="Password" type="password"  className="loginInput"  required/>
               <button className="loginButton">Log In</button>
               <span className="loginForgot">Forgot Password?</span>
               <button className="loginRegisterButton">
                 Create a New Account
               </button>
-            </div>
+            </form>
           </div>
         </div>
       </div>
