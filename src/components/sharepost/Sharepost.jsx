c97276972a68ebcdfa3fada01b16fb8e14a03b6b
salonibhatia-ttn <saloni.bhatia@tothenew.com>
2022-04-22 16:01:10 +0530

    Merge branch 'sub-frontend' of https://github.com/Sandeepbttn/ttnbuzz into sub-frontend

--- src/components/sharepost/Sharepost.jsx
+++ src/components/sharepost/Sharepost.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './sharepost.css'
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import {useContext} from 'react'
@@ -7,6 +7,15 @@ import { AuthContext } from "../../context/AuthContext";
 export default function Sharepost() {
 
   const { user } = useContext(AuthContext);
+  const desc = useRef();
+  const [file, setFile] = useState(null);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const addPost = {
+      userID: user._id,
+      desc: desc.current.value,
+    };  
+  }
   return (
 
         <div className='sharepost'>
@@ -17,9 +26,14 @@ export default function Sharepost() {
                 ? user.profilePicture
                 : "https://res.cloudinary.com/buzz-snaps/image/upload/v1650453947/noAvatar_mddqh4.png"
             } alt="profile image" />
-                        <input placeholder="Start a post" className="sharepostInput"/>
-                        <AddPhotoAlternateIcon className="sharepostIcon"/>
+                        <input placeholder=" Start a post..." className="sharepostInput" ref={desc}/>
+                        <form className='share' onSubmit={submitHandler}>
+                        <label htmlFor="file" className="sharepostOption" >
+                        <AddPhotoAlternateIcon htmlColor='green' className="sharepostIcon"/>  
                         <span className="sharepostOptionText">Photo/Video</span>
+                        <input style={{display: "none"}} type = "file" id="file" accept='.png,.jpeg,.jpg' onChange={(e) => setFile(e.target.files[0])} />
+                        </label>
+                        </form>
                     </div>      
             </div>
         </div>
